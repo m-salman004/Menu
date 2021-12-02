823888a58aeb3e8a291c728d28d5085ae0316a2f
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2021-12-01 23:39:44 -0800

    Add id: 4 to 7

--- aap.js
+++ aap.js
@@ -23,3 +23,40 @@ const menu = [
       img: "./images/item-3.jpeg",
       desc: `Ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
     },
+    {
+        id: 4,
+        title: "Country Delight",
+        category: "breakfast",
+        price: 20.99,
+        img: "./images/item-4.jpeg",
+        desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+      },
+      {
+        id: 5,
+        title: "Egg Attack",
+        category: "lunch",
+        price: 22.99,
+        img: "./images/item-5.jpeg",
+        desc: `Franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+      },
+      {
+        id: 6,
+        title: "Oreo Dream",
+        category: "shakes",
+        price: 18.99,
+        img: "./images/item-6.jpeg",
+        desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+      },
+      {
+        id: 7,
+        title: "Bacon Overflow",
+        category: "breakfast",
+        price: 8.99,
+        img: "./images/item-7.jpeg",
+        desc: `Carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+      },
+
+
+
+
+
